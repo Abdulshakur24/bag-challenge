3596bdd7431a1744f6c90bb524b11a51b43d5cd0
abdulshakur <abdulshakur486@gmail.com>
2022-02-21 22:22:55 +0300

    initial commit.

---
+++ server/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, 'Name required!'],
+      maxlength: 32,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: [true, 'Email required!'],
+      unique: true,
+    },
+    hashed_password: {
+      type: String,
+      required: [true, 'Password required!'],
+    },
+  },
+
+  { timestamps: true },
+)
+
+module.exports = mongoose.model('User', userSchema)
