0fc9bf469ee340b2146ff05245cebbab43b5f8bb
abdulshakur <abdulshakur486@gmail.com>
2022-02-25 21:57:15 +0300

    animations.

---
+++ client/src/manifest/motions.js
@@ -0,0 +1,5 @@
+export const pageMotion = {
+  initial: { opacity: 0, x: 0 },
+  animate: { opacity: 1, x: 0, transition: { duration: 0.25 } },
+  exit: { opacity: 0, x: 0, transition: { duration: 0.25 } },
+}
