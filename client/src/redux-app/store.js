347e2ef942f3faddc097c1189d7e6c033e649765
abdulshakur <abdulshakur486@gmail.com>
2022-02-26 21:07:55 +0300

    redux store.

---
+++ client/src/redux-app/store.js
@@ -0,0 +1,41 @@
+import { configureStore } from '@reduxjs/toolkit'
+// import { persistReducer,
+// FLUSH,
+// REHYDRATE,
+// PAUSE,
+// PERSIST,
+// PURGE,
+// REGISTER,
+//} from 'redux-persist'
+// import storage from 'redux-persist/lib/storage'
+import userReducer from './slicers/user'
+import toVisitReducer from './slicers/toVisit'
+import visitReducer from './slicers/visited'
+
+// const persistConfig = {
+//   key: 'root',
+//   version: 1,
+//   storage,
+// }
+
+// const rootReducer = combineReducers({
+//   user: userReducer,
+//   home: homeReducer,
+//   visit: visitReducer,
+// })
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export default configureStore({
+  reducer: {
+    user: userReducer,
+    toVisit: toVisitReducer,
+    visited: visitReducer,
+  },
+  // middleware: (getDefaultMiddleware) =>
+  //   getDefaultMiddleware({
+  //     serializableCheck: {
+  //       ignoreActions: [],
+  //     },
+  //   }),
+})
