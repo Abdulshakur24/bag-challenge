d9346fb390b490e5f799b8bb77cbc7d2348adab3
abdulshakur <abdulshakur486@gmail.com>
2022-02-26 21:09:35 +0300

    User state.

---
+++ client/src/redux-app/slicers/user.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    data: null,
+  },
+  reducers: {
+    loadUser: (state, { payload }) => {
+      state.data = payload
+      localStorage.setItem('token', payload.token)
+    },
+    signOut: (state) => {
+      state.data = null
+      localStorage.removeItem('token')
+    },
+  },
+})
+
+export const { loadUser, signOut } = userSlice.actions
+
+export default userSlice.reducer
