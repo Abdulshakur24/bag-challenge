fe202b81489797b22ad1d2065453da0970d0729b
abdulshakur <abdulshakur486@gmail.com>
2022-02-28 23:09:30 +0300

    initial commit.

---
+++ client/src/manifest/Home/HomeScreen.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+import { HomeWrapper } from "./HomeScreenStyle";
+import { useLocation } from "react-router-dom";
+
+import { Sidebar, Main } from ".";
+
+function Home() {
+  const { pathname } = useLocation();
+
+  useEffect(() => window.scrollTo({ top: 0, behavior: "smooth" }), [pathname]);
+
+  return (
+    <HomeWrapper>
+      <Sidebar />
+      <Main />
+    </HomeWrapper>
+  );
+}
+
+export default Home;
