adf874ca6d9e153c3be1facc05ff57f5d0262597
abdulshakur <abdulshakur486@gmail.com>
2022-03-02 21:59:14 +0300

    extended an expiration date.

--- server/controllers/helpers.js
+++ server/controllers/helpers.js
@@ -3,7 +3,7 @@ const client = redis.createClient()
 
 const setCache = async (key, value) => {
   await client.connect()
-  const result = await client.setEx(key, 3600 * 24, JSON.stringify(value))
+  const result = await client.setEx(key, 3600 * 24 * 7, JSON.stringify(value))
   await client.disconnect()
   if (result) return true
   return false
