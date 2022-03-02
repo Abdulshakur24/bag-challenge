b5ff18ba1981e534d7099308877690a0ecd6564a
abdulshakur <abdulshakur486@gmail.com>
2022-03-02 23:17:39 +0300

    initial commit.

---
+++ client/src/manifest/Home/components/Main/index.js
@@ -0,0 +1,4 @@
+export { default as MyList } from './MainComponents/MyList/MyList'
+export { default as ToVisit } from './MainComponents/ToVisit/ToVisit'
+export { default as Visited } from './MainComponents/Visited/Visited'
+export { default as Details } from './MainComponents/Details/Details'
