19f0013b675ed3eeb8a1c5091406971a32383cff
abdulshakur <abdulshakur486@gmail.com>
2022-03-03 22:22:14 +0300

    initial commit.

---
+++ client/src/manifest/Home/components/Main/Main.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Header } from "../..";
+import { MyList, ToVisit, Visited, Details } from ".";
+import { MainWrapper } from "./MainStyle";
+
+function Main() {
+  return (
+    <MainWrapper>
+      <Header />
+      <div className="routes-container">
+        <Routes>
+          <Route path="to-visit" element={<ToVisit />} />
+          <Route index path="to-visit/:name" element={<Details />} />
+          <Route path="my-list" element={<MyList />} />
+          <Route path="my-list/:name" element={<Details />} />
+          <Route path="visited" element={<Visited />} />
+          <Route path="visited/:name" element={<Details />} />
+          <Route path="*" element={<h1>Not found.</h1>} />
+        </Routes>
+      </div>
+    </MainWrapper>
+  );
+}
+
+export default Main;
