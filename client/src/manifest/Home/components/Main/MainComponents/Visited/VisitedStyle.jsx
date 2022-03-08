a79f2e683b2244ef4b54589deacb96bcb5bcd2a8
abdulshakur <abdulshakur486@gmail.com>
2022-03-08 19:43:11 +0300

    improved features.

--- client/src/manifest/Home/components/Main/MainComponents/Visited/VisitedStyle.jsx
+++ client/src/manifest/Home/components/Main/MainComponents/Visited/VisitedStyle.jsx
@@ -5,8 +5,10 @@ export const VisitedWrapper = styled(motion.div)`
   display: grid;
   grid-template: 1fr / repeat(auto-fit, minmax(328px, 1fr));
   gap: 2.5rem;
-  margin: 1rem 3.375rem;
+  justify-items: center;
+  margin: 1rem 0;
   @media ${(props) => props.theme.breakpoints.lg} {
     grid-template: 1fr / repeat(auto-fit, minmax(260px, 1fr));
+    margin: 1rem 3.375rem;
   }
 `;
