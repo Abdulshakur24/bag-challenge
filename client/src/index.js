604bff14ae2c59ec707a6d9eb1f45060f4e0bbbc
abdulshakur <abdulshakur486@gmail.com>
2022-03-10 11:21:32 +0300

    added persistence redux.

--- client/src/index.js
+++ client/src/index.js
@@ -8,18 +8,24 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { fetchCountries } from './redux-app/slicers/toVisit'
+import { PersistGate } from 'reduxjs-toolkit-persist/integration/react'
+import { persistStore } from 'reduxjs-toolkit-persist'
 
 store.dispatch(fetchCountries('europe'))
+
+const persistor = persistStore(store)
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Theme>
-        <Router>
-          <ToastContainer />
-          <App />
-        </Router>
-      </Theme>
+      <PersistGate loading={true} persistor={persistor}>
+        <Theme>
+          <Router>
+            <ToastContainer />
+            <App />
+          </Router>
+        </Theme>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
