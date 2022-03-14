15fc29b801b289594d5b3769619f9b9841d3ce94
abdulshakur <abdulshakur486@gmail.com>
2022-03-14 18:35:37 +0300

    rearranged file structure.

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,8 +1,7 @@
 export { default as Header } from './Header/Header'
-export { default as Main } from '../pages/Main/Main'
+export { default as Main } from './Main/Main'
 export { default as Sidebar } from './Sidebar/Sidebar'
-
 export { default as MyList } from './MyList/MyList'
 export { default as ToVisit } from './ToVisit/ToVisit'
 export { default as Visited } from './Visited/Visited'
-export { default as Details } from '../pages/Details/Details'
+export { default as Details } from './Details/Details'
