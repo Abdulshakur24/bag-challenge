2794eea0d17576a2c82fc75cd2772967f4187229
abdulshakur <abdulshakur486@gmail.com>
2022-03-23 22:28:00 +0300

    prettier formatted.

--- server/app.js
+++ server/app.js
@@ -1,56 +1,56 @@
-const express = require('express')
-const createError = require('http-errors')
-const morgan = require('morgan')
-require('dotenv').config()
-const mongoose = require('mongoose')
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
-const { body } = require('express-validator')
-const app = express()
-const path = require('path')
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(morgan('dev'))
-app.use(cookieParser())
-app.use(body())
-const isProduction = process.env.NODE_ENV === 'production'
-app.use(cors({ origin: isProduction ? '' : 'http://localhost:3000' }))
+const express = require("express");
+const createError = require("http-errors");
+const morgan = require("morgan");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const { body } = require("express-validator");
+const app = express();
+const path = require("path");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(body());
+const isProduction = process.env.NODE_ENV === "production";
+app.use(cors({ origin: isProduction ? "" : "http://localhost:3000" }));
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('Database connected!'))
+  .then(() => console.log("Database connected!"))
   .catch((err) => {
-    console.error('app.js ' + err)
-  })
+    console.error("app.js " + err);
+  });
 
-app.use('/api', require('./routes/apiRouter'))
+app.use("/api", require("./routes/apiRouter"));
 
 if (isProduction) {
-  app.use(express.static(path.join(__dirname, '../client/build')))
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build/index.html'))
-  })
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+  });
 } else {
-  app.get('/', (req, res) =>
-    res.send(`Please set your environment to 'production'.`),
-  )
+  app.get("/", (req, res) =>
+    res.send(`Please set your environment to 'production'.`)
+  );
 }
 
 app.use((req, res, next) => {
-  next(createError.NotFound())
-})
+  next(createError.NotFound());
+});
 
 app.use((err, req, res, next) => {
-  res.status(err.status || 500)
+  res.status(err.status || 500);
   res.send({
     status: err.status || 500,
     message: err.message,
-  })
-})
+  });
+});
 
-const PORT = process.env.PORT || 5010
+const PORT = process.env.PORT || 5010;
 app.listen(PORT, () =>
-  console.log(`Listening on port http://localhost:${PORT}`),
-)
+  console.log(`Listening on port http://localhost:${PORT}`)
+);
