40e79648b5e59c61b9844062a6374e1878d7d7a8
abdulshakur <abdulshakur486@gmail.com>
2022-03-28 17:11:04 +0300

    exports handlers.

---
+++ client/src/redux-app/slicers/index.js
@@ -0,0 +1,4 @@
+export { default as userReducer } from "./user";
+export { default as toVisitReducer } from "./toVisit";
+export { default as visitedReducer } from "./visited";
+export { default as listReducer } from "./list";
