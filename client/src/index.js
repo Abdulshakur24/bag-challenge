130e9229f1ce4a22a0e923d0b8c3c3b3a81bc12f
abdulshakur <abdulshakur486@gmail.com>
2022-04-09 11:08:24 +0300

    upgraded to react v18.0

--- client/src/index.js
+++ client/src/index.js
@@ -1,32 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './pages/App'
-import { Provider } from 'react-redux'
-import store from './redux-app/store'
-import Theme from './styles/Theme'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import { fetchCountries } from './redux-app/slicers/toVisit'
-import { PersistGate } from 'reduxjs-toolkit-persist/integration/react'
-import { persistStore } from 'reduxjs-toolkit-persist'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./pages/App";
+import { Provider } from "react-redux";
+import store from "./redux-app/store";
+import Theme from "./styles/Theme";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { fetchCountries } from "./redux-app/slicers/toVisit";
+import { PersistGate } from "reduxjs-toolkit-persist/integration/react";
+import { persistStore } from "reduxjs-toolkit-persist";
 
-store.dispatch(fetchCountries('europe'))
+store.dispatch(fetchCountries("europe"));
 
-const persistor = persistStore(store)
+const persistor = persistStore(store);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={true} persistor={persistor}>
-        <Theme>
-          <Router>
-            <ToastContainer />
-            <App />
-          </Router>
-        </Theme>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root'),
-)
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <PersistGate loading={true} persistor={persistor}>
+      <Theme>
+        <Router>
+          <ToastContainer />
+          <App />
+        </Router>
+      </Theme>
+    </PersistGate>
+  </Provider>
+);
