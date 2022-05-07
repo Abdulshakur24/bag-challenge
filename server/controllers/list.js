23754cf564265cefa3c9bfee14f59126d029fe43
abdulshakur <abdulshakur486@gmail.com>
2022-05-07 19:46:55 +0300

    refractored.

--- server/controllers/list.js
+++ server/controllers/list.js
@@ -1,47 +1,47 @@
-const List = require('../models/list')
-const jwt = require('jsonwebtoken')
+const List = require("../models/list");
+const jwt = require("jsonwebtoken");
 
 const extractUserIdFromToken = (authorization) => {
-  const token = authorization.split(' ')[1]
-  return jwt.verify(token, process.env.JWT_KEY).user._id
-}
+  const token = authorization.split(" ")[1];
+  return jwt.verify(token, process.env.JWT_KEY).user._id;
+};
 
 const getAllLists = async (authorization) => {
-  const userId = extractUserIdFromToken(authorization)
-  return await List.find({ user: userId })
-}
+  const userId = extractUserIdFromToken(authorization);
+  return await List.find({ user: userId });
+};
 
 const postList = async (authorization, post) => {
-  const userId = extractUserIdFromToken(authorization)
+  const userId = extractUserIdFromToken(authorization);
   const objectExist = await List.findOne({
     user: userId,
-    'name.common': post.name.common,
-  })
-  if (objectExist) return objectExist
-  const response = await List.create({ user: userId, ...post })
-  return response
-}
+    area: post.area,
+  });
+
+  if (objectExist) return objectExist;
+  return await List.create({ user: userId, ...post });
+};
 const updateList = async (authorization, { visited }, id, res) => {
-  const userId = extractUserIdFromToken(authorization)
+  const userId = extractUserIdFromToken(authorization);
   const { acknowledged } = await List.findById(id)
     .where({ user: userId })
-    .updateOne({ visited: Boolean(visited) })
+    .updateOne({ visited: Boolean(visited) });
   if (acknowledged) {
-    return res.send({ message: 'Updated.', id, visited: visited })
+    return res.send({ message: "Updated.", id, visited: visited });
   }
-  return res.status(403).send('Update failed.')
-}
+  return res.status(403).send("Update failed.");
+};
 
 const deleteList = async (authorization, id, res) => {
-  const userId = extractUserIdFromToken(authorization)
-  const { deletedCount } = await List.findById(id).deleteOne({ user: userId })
-  if (!deletedCount) return res.status(403).send('No object found to delete.')
-  return res.send({ id, message: 'Successfully deleted.' })
-}
+  const userId = extractUserIdFromToken(authorization);
+  const { deletedCount } = await List.findById(id).deleteOne({ user: userId });
+  if (!deletedCount) return res.status(403).send("No object found to delete.");
+  return res.send({ id, message: "Successfully deleted." });
+};
 
 module.exports = {
   getAllLists,
   postList,
   updateList,
   deleteList,
-}
+};
