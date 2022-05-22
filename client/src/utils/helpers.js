4bb7edcafbd10de6aaad2f2a3a5c8b8057b19c15
abdulshakur <abdulshakur486@gmail.com>
2022-05-22 15:06:16 +0300

    added new axios

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,3 +1,9 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://restcountries.com/v3.1",
+});
+
 export const getBase64 = (file) => {
   return new Promise((resolve) => {
     let reader = new FileReader();
@@ -7,3 +13,5 @@ export const getBase64 = (file) => {
     };
   });
 };
+
+export const fetchCountryByName = (query) => instance.get(`/name/${query}`);
