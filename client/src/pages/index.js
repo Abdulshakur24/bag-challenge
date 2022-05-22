1fc1983fbe442aa710f3b0f5031c693f58de1e28
abdulshakur <abdulshakur486@gmail.com>
2022-05-22 14:26:36 +0300

    add new exports

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,3 +1,5 @@
+export { default as Register } from "./Register/Register";
+export { default as Login } from "./Login/Login";
 export { default as ToVisit } from "./ToVisit/ToVisit";
 export { default as MyList } from "./MyList/MyList";
 export { default as Visited } from "./Visited/Visited";
