318ef93d62368f10ce878f80bfed6c12d7f023fc
abdulshakur <abdulshakur486@gmail.com>
2022-05-22 14:47:43 +0300

    added new exports.

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,4 @@
-export { default as Login } from "./Login/Login";
-export { default as Register } from "./Register/Register";
 export { default as Preferences } from "./Preferences/Preferences";
 export { default as Settings } from "./Settings/Settings";
 export { default as Country } from "./Country/Country";
+export { default as SearchModal } from "./SearchModal/SearchModal";
