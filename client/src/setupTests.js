c1927743a0d5af57a62db14df04ab9997a1883af
abdulshakur <abdulshakur486@gmail.com>
2022-05-23 11:32:33 +0300

    wrapped all providers in render function.

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,5 +1,25 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import Theme from "./components/Theme";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const AllTheProviders = ({ children }) => {
+  return (
+    <Provider store={store}>
+      <Theme>
+        <Router>{children}</Router>
+      </Theme>
+    </Provider>
+  );
+};
+
+const customRender = (ui, options) =>
+  render(ui, { wrapper: AllTheProviders, ...options });
+
+// re-export everything
+export * from "@testing-library/react";
+
+// override render method
+export { customRender as render };
