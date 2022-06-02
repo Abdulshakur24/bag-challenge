7b16ffd08201b3ab4fd45f97ca33229671207155
abdulshakur <abdulshakur486@gmail.com>
2022-06-02 23:28:04 +0300

    logs.

--- server/preventDynosFromIdling.js
+++ server/preventDynosFromIdling.js
@@ -1,20 +1,24 @@
 const { default: axios } = require("axios");
 
-// By default, Heroku dynos ildes after half an hour of inactivity.
-// so we want to ping these dynos after every 29 minutes, leaving no chance for dynos to idle.
+// By default, Heroku dyno ildes after half an hour of inactivity.
+// so we want to ping these dyno after every 29 minutes, leaving no chance for dynos to idle.
 function startKeepAlive() {
+  let counts = 0;
   setInterval(function () {
     const urls = [
       "https://audiophile-e-commerce.herokuapp.com",
       "http://bag-2022.herokuapp.com",
     ];
 
+    console.log(`request made: ${counts}`);
+
     for (let i = 0; i < urls.length; i++) {
+      counts += 1;
       axios
         .get(urls[i], { method: "GET" })
         .then((data) => console.log(data.config.url));
     }
-  }, 28 * 60 * 1000); // load every 58 minutes
+  }, 29 * 60 * 1000); // load every 58 minutes
 }
 
 startKeepAlive();
