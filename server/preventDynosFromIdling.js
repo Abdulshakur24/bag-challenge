012e6543ae4b8a8de414e24581fcecf7b72d73bc
abdulshakur <abdulshakur486@gmail.com>
2022-06-02 17:27:24 +0300

    reduced to 29 minutes

--- server/preventDynosFromIdling.js
+++ server/preventDynosFromIdling.js
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios");
 
-// By default, Heroku dynos ildes after one hour of inactivity.
-// so we want to ping these dynos after every 58 minutes, leaving no chance for dynos to idle.
+// By default, Heroku dynos ildes after half an hour of inactivity.
+// so we want to ping these dynos after every 29 minutes, leaving no chance for dynos to idle.
 function startKeepAlive() {
   setInterval(function () {
     const urls = [
@@ -14,7 +14,7 @@ function startKeepAlive() {
         .get(urls[i], { method: "GET" })
         .then((data) => console.log(data.config.url));
     }
-  }, 58 * 60 * 1000); // load every 58 minutes
+  }, 28 * 60 * 1000); // load every 58 minutes
 }
 
 startKeepAlive();
