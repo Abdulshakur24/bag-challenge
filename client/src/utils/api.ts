6d9f67f77599c24ecf264c473f6c6924e3306238
abdulshakur <abdulshakur486@gmail.com>
2022-06-13 11:59:04 +0300

    create API using RTK query.

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,4 +1,6 @@
 import axios, { AxiosInstance } from "axios";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { fetchCountries } from "src/redux/slicers/toVisit";
 
 export const thirdPartyAPI: AxiosInstance = axios.create({
   baseURL: "https://restcountries.com/v3.1",
@@ -9,3 +11,21 @@ const isProduction = process.env.NODE_ENV === "production";
 export const restfulAPI: AxiosInstance = axios.create({
   baseURL: isProduction ? "api/" : "http://localhost:5010/api/",
 });
+
+export const restcountriesAPI = createApi({
+  reducerPath: "restcountriesAPI",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://restcountries.com/v3.1" }),
+  endpoints: (builder) => ({
+    getByRegion: builder.query({
+      query: (region) => `region/${region}`,
+      async onQueryStarted(region, { dispatch }) {
+        dispatch(fetchCountries("africa"));
+        console.log(region);
+      },
+    }),
+  }),
+});
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useGetByRegionQuery } = restcountriesAPI;
